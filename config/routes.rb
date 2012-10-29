ac12aed3a4799f3539b78b6ac657d26abd21bd03
Mario <mario@crunchbang.(none)>
2012-10-29 13:11:25 +0200

    Committing haml views with parser and new Comments model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Blog::Application.routes.draw do
   resources :posts
+  resources :comments
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -50,7 +51,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
